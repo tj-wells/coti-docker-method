ad7f0cbe5492e27b749c0be4e62304cfaebc855d
Tomos Wells <tomosjuleswells@gmail.com>
2023-10-09 20:14:17 +0000

    Add unsync check to node-monitor

---
+++ node-monitor.sh
@@ -0,0 +1,55 @@
+apk add curl jq > /dev/null
+
+network="$1"                                # mainnet | testnet
+node_url="$2"                               # e.g. testnet.your-node.com
+sync_ref_node_url=testnet-trustscore1.coti.io
+unsync_tolerance=10
+RESTART_COMMAND="docker-compose -f /coti-node/docker-compose.yml restart coti-node"
+
+function get_last_index() {
+  local response=$(curl -s "https://$1/transaction/lastIndex")
+  local last_index=$(echo "$response" | jq -r '.lastIndex')
+  echo "$last_index"
+}
+
+function restart_if_unsynced() {
+  echo "Performing sync check: $(date '+%A %d %m %Y %X')"
+  local node_url=$1
+  local sync_ref_node_url=$2
+  local unsync_tolerance=$3
+  local node_last_index=$(get_last_index "$node_url")
+  local sync_ref_node_last_index=$(get_last_index "$sync_ref_node_url")
+
+  # Check $node_last_index and $sync_ref_node_last_index are integers
+  if [[ -z "$node_last_index" || -z "$sync_ref_node_last_index" ]]; then
+    echo "Error getting last_index. Try again later"
+    return 1
+  fi
+
+  if [ $((sync_ref_node_last_index - node_last_index)) -gt "$unsync_tolerance" ]; then
+    echo "Node is unsynced. Performing restart."
+    $RESTART_COMMAND
+  else
+    echo "Node is synced."
+  fi
+}
+
+while true; do
+  sleep 600
+  echo "Performing status check: $(date '+%A %d %m %Y %X')"
+  status_code=$(curl -o /dev/null -s -w '%{http_code}' https://${network}-nodemanager.coti.io/nodes)
+
+
+  if [ "$status_code" -eq 200 ]; then
+    if curl -s https://${network}-nodemanager.coti.io/nodes | grep -q ${node_url}; then
+      echo "Node ${node_url} is connected."
+      restart_if_unsynced "$node_url" "$sync_ref_node_url" "$unsync_tolerance"
+    else
+      echo "Node not found. Performing restart."
+      $RESTART_COMMAND
+    fi
+  else
+    echo "Node manager returned unusual status code: $status_code"
+  fi
+done
+
